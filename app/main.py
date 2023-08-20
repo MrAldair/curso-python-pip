b45537857a4748080a9fd5ca2b283acbb5532188
MrAldair <55173520+MrAldair@users.noreply.github.com>
2023-08-20 14:00:15 -0700

    Actualización Pandas

--- app/main.py
+++ app/main.py
@@ -1,19 +1,29 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('./data.csv')
+  """""
+  
   
   continent = input('Escoge un continente: ')
 
   data = list(filter(lambda item : item['Continent'] == continent, data))
+
   countries = list(map(lambda pais: pais['Country/Territory'], data))
   percentages = list(map(lambda porcentaje: porcentaje['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
   
- 
-  country = input("Digite un pais: ")
+  """
+  df = pd.read_csv('./data.csv')
+  df = df[df["Continent"] == "Africa"]
+
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+
+  data = read_csv.read_csv('./data.csv')
+  country = input("Escoge un país: ")
   print(country)
 
   result = utils.population_by_country(data, country)
