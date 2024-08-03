0a77e66a0e910e77229c23c6049512af58f21e1e
MrAldair <55173520+MrAldair@users.noreply.github.com>
2024-08-03 13:45:51 -0700

    Importanción de Jinja2, creacion de template de pantalla contacts

--- crudFastAPI/main.py
+++ crudFastAPI/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse,FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from uuid import uuid4 as uuid
 from pydantic import BaseModel
 
@@ -16,17 +19,32 @@ class ContactModel(BaseModel):
 
 app = FastAPI()
 
-@app.get('/')
+app.mount("/static", StaticFiles(directory="./public/static"), name="static")
+
+templates = Jinja2Templates(directory="./public/templates")
+
+@app.get('/', response_class=HTMLResponse)
 def root():
-    return {"message": "I am FastAPI"}
+    html_address = "./public/static/html/index.html"
+    return FileResponse(html_address, status_code=200)
 
-@app.get("/api/contacts")
-def get_all_contacts():
-    return get_contacts()
+@app.get("/api/contacts", response_class=HTMLResponse)
+def get_all_contacts(request: Request):
+    contacts = get_contacts()
+    return templates.TemplateResponse("contacts.html", {"request":request, "contacts": contacts}, status_code=200)
 
-@app.get("/api/contacts/{id_contact}")
-def get_single_contact(id_contact:str):
-    return get_contact(id_contact)
+#Trabajando aqui
+#@app.get("/api/contacts/{id_contact}", response_class="HTMLResponse")
+#def get_single_contact(request: Request, id_contact:str):
+#    return get_contact(id_contact)
+    #return templates.TemplateResponse("item.html",{"request": request, "id_contact": id_contact})
+
+@app.get("/api/contacts/{id_contact}", response_class=HTMLResponse)
+def get_single_contact(request: Request, id_contact: str):
+    contact = get_contact(id_contact)
+    return templates.TemplateResponse("item.html", {"request": request, "contact": contact})
+
+
 
 @app.post("/api/contacts")
 def add_contact(new_contact: ContactModel):
